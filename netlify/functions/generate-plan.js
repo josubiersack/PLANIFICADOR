15ccfaa3e41a5b121e4dea80f64d523b86f8334a
josubiersack <josuecruzzambrano77@gmail.com>
2026-08-22 20:50:52 -0500

    Update generate-plan.js

--- netlify/functions/generate-plan.js
+++ netlify/functions/generate-plan.js
@@ -106,7 +106,7 @@ IMPORTANTE:
 - El tiempo total de actividades debe sumar exactamente ${tiempo} minutos
 `;
   const modelos = [
-    { id: "meta-llama/llama-4-scout-17b-16e-instruct", maxTk: 3000 },
+    { id: "llama3-70b-8192", maxTk: 3000 },
     { id: "llama-3.1-8b-instant", maxTk: 2000 },
   ];
 
